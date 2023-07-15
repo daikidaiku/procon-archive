624adce67577db7c44fc7e92ef6e383bec3f7a63
daikidaiku <daikimori990407@gmail.com>
2023-07-15 14:11:17 +0000

    [AC] abc307 abc307_a

---
+++ atcoder.jp/abc307/abc307_a/Main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+const long long INF = numeric_limits<long long>::max();
+int main()
+{
+    int N;
+    cin >> N;
+    int A[7*N];
+    rep(i,7*N)cin >> A[i];
+    rep(i,N){
+        int sum=0;
+        rep(j,7){
+            sum = sum + A[j+7*i]; 
+        }
+        cout << sum << endl;
+    }
+    return 0;
+}
