1904309bdd0bd722bcb81c59a4923204eb8531f3
daikidaiku <daikimori990407@gmail.com>
2023-07-21 05:06:55 +0000

    [AC] abc228 abc228_a

---
+++ atcoder.jp/abc228/abc228_a/Main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define all(...) begin(__VA_ARGS__), end(__VA_ARGS__)
+#define rall(...) rbegin(__VA_ARGS__), rend(__VA_ARGS__)
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+const long long INF = numeric_limits<long long>::max();
+int main()
+{
+    int S, T, X;
+    cin >> S >> T >> X;
+    if(T>S){
+        if(S <= X && X < T){
+            cout << "Yes" << endl;
+        }else{
+            cout << "No" << endl;
+        }
+    }else{
+        if(S <= X || X < T){
+            cout << "Yes" << endl;
+        }else{
+            cout << "No" << endl;
+        }
+
+    }
+    return 0;
+}
