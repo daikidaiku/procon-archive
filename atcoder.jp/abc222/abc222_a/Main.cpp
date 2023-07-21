db19ddab3ac2e036b091d82b18d9aeb2e4063d3e
daikidaiku <daikimori990407@gmail.com>
2023-07-21 01:13:31 +0000

    [AC] abc222 abc222_a

---
+++ atcoder.jp/abc222/abc222_a/Main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define all(...) begin(__VA_ARGS__), end(__VA_ARGS__)
+#define rall(...) rbegin(__VA_ARGS__), rend(__VA_ARGS__)
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+const long long INF = numeric_limits<long long>::max();
+int main()
+{
+    string N, ans;
+    cin >> N;
+    int len;
+    len = N.length();
+    if(len < 4){
+        ans = N;
+        rep (i,4 - len){
+            ans = "0" + ans;
+        }
+    }else{
+        ans = N;
+    }
+    cout << ans << endl;
+    return 0;
+}
