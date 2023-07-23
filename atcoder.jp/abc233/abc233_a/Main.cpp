d02c662b9fc7eff90bb94b5d9c51cc2ba129688c
daikidaiku <daikimori990407@gmail.com>
2023-07-23 17:26:49 +0000

    [AC] abc233 abc233_a

---
+++ atcoder.jp/abc233/abc233_a/Main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define all(...) begin(__VA_ARGS__), end(__VA_ARGS__)
+#define rall(...) rbegin(__VA_ARGS__), rend(__VA_ARGS__)
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+const long long INF = numeric_limits<long long>::max();
+int main()
+{
+    int X, Y, ans = 0;
+    cin >> X >> Y;
+    while (X < Y)
+    {
+        X+=10;
+        ans++;
+    }
+    cout << ans << endl;
+    
+    return 0;
+}
