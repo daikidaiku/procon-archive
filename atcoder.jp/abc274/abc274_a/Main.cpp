807a681feb94f54696f5ecef81e1f5682bff7641
daikidaiku <daikimori990407@gmail.com>
2023-07-24 14:23:19 +0000

    [AC] abc274 abc274_a

---
+++ atcoder.jp/abc274/abc274_a/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define all(...) begin(__VA_ARGS__), end(__VA_ARGS__)
+#define rall(...) rbegin(__VA_ARGS__), rend(__VA_ARGS__)
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+const long long INF = numeric_limits<long long>::max();
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    float ans = (float) b/a;
+    cout << fixed << setprecision(3) << ans << endl;
+    return 0;
+}
