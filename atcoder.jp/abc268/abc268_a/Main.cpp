23a43cfaa78329a9edd03c78c18dd4ca046f36d3
daikidaiku <daikimori990407@gmail.com>
2023-07-24 13:05:23 +0000

    [AC] abc268 abc268_a

---
+++ atcoder.jp/abc268/abc268_a/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define all(...) begin(__VA_ARGS__), end(__VA_ARGS__)
+#define rall(...) rbegin(__VA_ARGS__), rend(__VA_ARGS__)
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+const long long INF = numeric_limits<long long>::max();
+int main()
+{
+    vector<int> a(5);
+    rep(i,5)cin >> a.at(i);
+    sort(all(a));
+    a.erase(unique(all(a)), a.end());
+    cout << a.size() << endl;
+    return 0;
+}
