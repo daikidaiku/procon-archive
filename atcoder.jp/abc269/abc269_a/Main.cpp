6eb58356173dabe49f47e24f7eb7ca1a59ee2ce1
daikidaiku <daikimori990407@gmail.com>
2023-07-24 13:23:16 +0000

    [AC] abc269 abc269_a

---
+++ atcoder.jp/abc269/abc269_a/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define all(...) begin(__VA_ARGS__), end(__VA_ARGS__)
+#define rall(...) rbegin(__VA_ARGS__), rend(__VA_ARGS__)
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+const long long INF = numeric_limits<long long>::max();
+int main()
+{
+    int a, b, c, d, ans;
+    cin >> a >> b >> c >> d;
+    ans = (a+b) * (c-d);
+    cout << ans << endl;
+    cout << "Takahashi" << endl;
+    return 0;
+}
