7374d6e7ee77e9912c7ff58c444135bb8e3d14d0
daikidaiku <daikimori990407@gmail.com>
2023-07-24 14:06:55 +0000

    [AC] abc272 abc272_a

---
+++ atcoder.jp/abc272/abc272_a/Main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define all(...) begin(__VA_ARGS__), end(__VA_ARGS__)
+#define rall(...) rbegin(__VA_ARGS__), rend(__VA_ARGS__)
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+const long long INF = numeric_limits<long long>::max();
+int main()
+{
+    int n, sum = 0;
+    cin >> n;
+    int a[n];
+    rep(i,n) cin >> a[i];
+    rep(i,n) sum += a[i];
+    cout << sum << endl;
+    return 0;
+}
