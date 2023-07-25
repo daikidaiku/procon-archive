453d5c9db4ad5b6ca49eb529996857445b66f513
daikidaiku <daikimori990407@gmail.com>
2023-07-25 09:03:29 +0000

    [AC] abc285 abc285_a

---
+++ atcoder.jp/abc285/abc285_a/Main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define all(...) begin(__VA_ARGS__), end(__VA_ARGS__)
+#define rall(...) rbegin(__VA_ARGS__), rend(__VA_ARGS__)
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+const long long INF = numeric_limits<long long>::max();
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if(2*a == b || 2*a + 1 == b){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
+    return 0;
+}
