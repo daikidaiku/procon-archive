e5cdd8539e094419f00c0b0ee7057d647c29ec58
daikidaiku <daikimori990407@gmail.com>
2023-08-16 13:42:46 +0000

    [AC] abc314 abc314_a

---
+++ atcoder.jp/abc314/abc314_a/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define all(...) begin(__VA_ARGS__), end(__VA_ARGS__)
+#define rall(...) rbegin(__VA_ARGS__), rend(__VA_ARGS__)
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+const long long INF = numeric_limits<long long>::max();
+int main()
+{
+    int n;
+    cin >> n;
+    string PI = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
+    rep(i,n+2) cout << PI[i];
+    cout << endl;
+    return 0;
+}
