09ce73d496333a64387179a697803d9f54c33e86
daikidaiku <daikimori990407@gmail.com>
2023-08-21 13:59:52 +0000

    [AC] abc315 abc315_a

---
+++ atcoder.jp/abc315/abc315_a/Main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define all(...) begin(__VA_ARGS__), end(__VA_ARGS__)
+#define rall(...) rbegin(__VA_ARGS__), rend(__VA_ARGS__)
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+const long long INF = numeric_limits<long long>::max();
+int main()
+{
+    string s;
+    cin >> s;
+    string ans = "";
+    for(char c : s){
+        if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u'){
+            ans += c;
+        }
+    }
+
+    cout << ans << endl;
+
+
+    return 0;
+}
