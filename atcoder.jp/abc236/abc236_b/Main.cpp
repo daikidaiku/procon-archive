01c3a04b0931431c35e5a5a17c85a692938cd611
daikidaiku <daikimori990407@gmail.com>
2023-08-30 11:57:58 +0000

    [AC] abc236 abc236_b

---
+++ atcoder.jp/abc236/abc236_b/Main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define all(...) begin(__VA_ARGS__), end(__VA_ARGS__)
+#define rall(...) rbegin(__VA_ARGS__), rend(__VA_ARGS__)
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+const long long INF = numeric_limits<long long>::max();
+int main()
+{
+    int n;
+    cin >> n;
+    int num = 4*n - 1;
+    int a[num], res = 0;
+    rep(i,num){
+        cin >> a[i];
+        res += a[i];
+    }
+
+    int ans = 0;
+    for (int i = 1; i <= n; i++){
+        ans += 4 * i;
+    }
+    ans -= res;
+    cout << ans << endl;
+    return 0;
+}
