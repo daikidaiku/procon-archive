f0be6b4e60762875792f382b4aa6401bfe7a06c0
daikidaiku <daikimori990407@gmail.com>
2023-08-30 12:15:45 +0000

    [AC] abc208 abc208_a

---
+++ atcoder.jp/abc208/abc208_a/Main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define all(...) begin(__VA_ARGS__), end(__VA_ARGS__)
+#define rall(...) rbegin(__VA_ARGS__), rend(__VA_ARGS__)
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+const long long INF = numeric_limits<long long>::max();
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    int max = 6 * a;
+    int min = a;
+    if(min <= b && b <= max) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
+}
