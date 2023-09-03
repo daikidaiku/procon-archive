418f5a4124b902299952597030b415b64fa6c3f8
daikidaiku <daikimori990407@gmail.com>
2023-09-03 05:32:47 +0000

    [AC] abc237 abc237_b

---
+++ atcoder.jp/abc237/abc237_b/Main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define all(...) begin(__VA_ARGS__), end(__VA_ARGS__)
+#define rall(...) rbegin(__VA_ARGS__), rend(__VA_ARGS__)
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+const long long INF = numeric_limits<long long>::max();
+int main()
+{
+    int h, w;
+    cin >> h >> w;
+    
+    long long a[w][h];
+    rep(i,h){
+        rep(j,w){
+            cin >> a[j][i];
+        }
+    }
+
+    rep(i,w){
+        rep(j,h){
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
