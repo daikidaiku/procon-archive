59bdd8cb5c44694c8496b11b662192cad905ea1f
daikidaiku <daikimori990407@gmail.com>
2023-09-03 04:38:22 +0000

    [AC] abc232 abc232_b

---
+++ atcoder.jp/abc232/abc232_b/Main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define all(...) begin(__VA_ARGS__), end(__VA_ARGS__)
+#define rall(...) rbegin(__VA_ARGS__), rend(__VA_ARGS__)
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+const long long INF = numeric_limits<long long>::max();
+int main()
+{
+    string s, t;
+    cin >> s >> t;
+    
+    rep(i,26){
+        rep(j,s.size()){
+            s[j] = s[j] + 1;
+            if (s[j] > 'z'){
+                s[j] -= 26;
+            }
+        }
+        if(s == t) {
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+    cout << "No" << endl;
+    return 0;
+}
