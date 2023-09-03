5eb62f6fd9dfa521bab24b1fb2a46f6d7479ac80
daikidaiku <daikimori990407@gmail.com>
2023-09-03 13:35:55 +0000

    [AC] abc203 abc203_a

---
+++ atcoder.jp/abc203/abc203_a/Main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define all(...) begin(__VA_ARGS__), end(__VA_ARGS__)
+#define rall(...) rbegin(__VA_ARGS__), rend(__VA_ARGS__)
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+const long long INF = numeric_limits<long long>::max();
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if(a == b) cout << c << endl;
+    else if(b == c) cout << a << endl;
+    else if(c == a) cout << b << endl;
+    else cout << 0 << endl;
+    
+    return 0;
+}
