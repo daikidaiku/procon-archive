f112c08aba47f6ab73bd81c314e842e6b338a397
daikidaiku <daikimori990407@gmail.com>
2023-09-04 13:49:27 +0000

    [AC] abc287 abc287_b

---
+++ atcoder.jp/abc287/abc287_b/Main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define all(...) begin(__VA_ARGS__), end(__VA_ARGS__)
+#define rall(...) rbegin(__VA_ARGS__), rend(__VA_ARGS__)
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+const long long INF = numeric_limits<long long>::max();
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    string s[n], t[m];
+    rep(i,n) cin >> s[i];
+    rep(i,m) cin >> t[i];
+
+    int res = 0;
+    rep (i, n) {
+        string z = s[i].substr(3);
+        // if()
+        rep (j, m) {
+            if (z == t[j]){
+                res++;
+                break;
+            }
+        }
+    }
+    cout << res << endl;
+    return 0;
+}
