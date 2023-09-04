2bf88ddb73d5d24ae5973f838e1462f5122416d6
daikidaiku <daikimori990407@gmail.com>
2023-09-04 14:16:52 +0000

    [AC] abc280 abc280_b

---
+++ atcoder.jp/abc280/abc280_b/Main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define all(...) begin(__VA_ARGS__), end(__VA_ARGS__)
+#define rall(...) rbegin(__VA_ARGS__), rend(__VA_ARGS__)
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+const long long INF = numeric_limits<long long>::max();
+int main()
+{
+    int n;
+    cin >> n;
+    ll s[100];
+    s[0] = 0;
+    rep2(i,1,n+1) cin >> s[i];
+    ll z;
+    ll a[100];
+    rep2(i,1,n+1){
+        a[i] = s[i] - s[i-1];
+    }
+
+    rep2(i, 1, n+1){
+        if(i != n) cout << a[i] << " ";
+        else cout << a[i] << endl;
+    }
+    return 0;
+}
