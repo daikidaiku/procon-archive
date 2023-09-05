f94e04a4f56b471c2967d0e15d4524f515dbf25a
daikidaiku <daikimori990407@gmail.com>
2023-09-05 13:12:10 +0000

    [AC] abc268 abc268_b

---
+++ atcoder.jp/abc268/abc268_b/Main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define all(...) begin(__VA_ARGS__), end(__VA_ARGS__)
+#define rall(...) rbegin(__VA_ARGS__), rend(__VA_ARGS__)
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+const long long INF = numeric_limits<long long>::max();
+int main()
+{
+    string s, t;
+    cin >> s >> t;
+    
+    int len = s.size();
+
+    bool res = true;
+    rep (i,len){
+        if (t[i] != s[i]) res = false;
+    }
+    if (res) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
+}
