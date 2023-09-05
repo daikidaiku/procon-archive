acca5ce2cb3a90ffa44333fe97fcc6983f4aac2c
daikidaiku <daikimori990407@gmail.com>
2023-09-05 14:01:42 +0000

    [AC] abc192 abc192_a

---
+++ atcoder.jp/abc192/abc192_a/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define all(...) begin(__VA_ARGS__), end(__VA_ARGS__)
+#define rall(...) rbegin(__VA_ARGS__), rend(__VA_ARGS__)
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+const long long INF = numeric_limits<long long>::max();
+int main()
+{
+    int x;
+    cin >> x;
+    
+    cout << 100-x%100 << endl;
+    return 0;
+}
