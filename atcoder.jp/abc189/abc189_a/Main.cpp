f16d11c0528d9533fc3a765cc92c024efadecc15
daikidaiku <daikimori990407@gmail.com>
2023-09-06 12:32:55 +0000

    [AC] abc189 abc189_a

---
+++ atcoder.jp/abc189/abc189_a/Main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define all(...) begin(__VA_ARGS__), end(__VA_ARGS__)
+#define rall(...) rbegin(__VA_ARGS__), rend(__VA_ARGS__)
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+const long long INF = numeric_limits<long long>::max();
+int main()
+{
+    string s;
+    cin >> s;
+    
+    if (s[0] == s[1] && s[1] == s[2]) cout << "Won" << endl;
+    else cout << "Lost" << endl;
+    return 0;
+}
