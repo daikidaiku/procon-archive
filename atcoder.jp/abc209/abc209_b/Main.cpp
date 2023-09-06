34784b188c3d5c7ab8ade78d46ad002bf056a3cc
daikidaiku <daikimori990407@gmail.com>
2023-09-06 12:44:54 +0000

    [AC] abc209 abc209_b

---
+++ atcoder.jp/abc209/abc209_b/Main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define all(...) begin(__VA_ARGS__), end(__VA_ARGS__)
+#define rall(...) rbegin(__VA_ARGS__), rend(__VA_ARGS__)
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+const long long INF = numeric_limits<long long>::max();
+int main()
+{
+    int n, x;
+    cin >> n >> x;
+    int a[110];
+    rep2(i,1,n+1) cin >> a[i];
+    
+    rep2(i,1,n+1){
+        if (i % 2){
+            x -= a[i];
+        }else{
+            x -= a[i] - 1;
+        }
+    }
+
+    if(x >= 0) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
+}
