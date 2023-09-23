bc352468e260cf97c1858fac66dc490549a0ab1a
daikidaiku <daikimori990407@gmail.com>
2023-09-23 11:20:56 +0000

    [AC] abc319 abc319_a

---
+++ atcoder.jp/abc319/abc319_a/Main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define all(...) begin(__VA_ARGS__), end(__VA_ARGS__)
+#define rall(...) rbegin(__VA_ARGS__), rend(__VA_ARGS__)
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+const long long INF = numeric_limits<long long>::max();
+int main()
+{
+    map<string, int> mp;
+    mp["tourist"] = 3858;
+    mp["ksun48"] = 3679;
+    mp["Benq"] = 3658;
+    mp["Um_nik"] = 3648;
+    mp["apiad"] = 3638;
+    mp["Stonefeang"] = 3630;
+    mp["ecnerwala"] = 3613;
+    mp["mnbvmar"] = 3555;
+    mp["newbiedmy"] = 3516;
+    mp["semiexp"] = 3481;
+    string s;
+    cin >> s;
+    
+    cout << mp[s] << endl;
+    return 0;
+}
