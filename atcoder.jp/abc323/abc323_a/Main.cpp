2d53c41c7b7245361709ec9dcce235ea03de6e98
daikidaiku <daikimori990407@gmail.com>
2023-11-06 04:14:15 +0000

    [AC] abc323 abc323_a

---
+++ atcoder.jp/abc323/abc323_a/Main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define all(...) begin(__VA_ARGS__), end(__VA_ARGS__)
+#define rall(...) rbegin(__VA_ARGS__), rend(__VA_ARGS__)
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+const long long INF = numeric_limits<long long>::max();
+int main()
+{
+    string s;
+    cin >> s;
+    bool res = true;
+    for (int i = 1; i < 16; i+=2){
+        if(s[i] != '0') res = false;
+    }
+
+    if(res){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
+    return 0;
+}
