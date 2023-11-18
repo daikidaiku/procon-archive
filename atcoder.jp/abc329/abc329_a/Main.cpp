cf8b56d50af2c251e9b89cb63035c03a9a5fe01d
daikidaiku <daikimori990407@gmail.com>
2023-11-18 12:02:08 +0000

    [AC] abc329 abc329_a

---
+++ atcoder.jp/abc329/abc329_a/Main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define all(...) begin(__VA_ARGS__), end(__VA_ARGS__)
+#define rall(...) rbegin(__VA_ARGS__), rend(__VA_ARGS__)
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+const long long INF = numeric_limits<long long>::max();
+int main()
+{
+    string s;
+    cin >> s;
+    int len = s.size();
+
+    rep(i,len){
+        if(i != len-1) cout << s[i] << " ";
+        else cout << s[i] << endl;
+    }
+
+    return 0;
+}
