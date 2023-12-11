e5180cd4cad701df07c396c89dcf2e060b812331
daikidaiku <daikimori990407@gmail.com>
2023-12-11 02:36:00 +0000

    [AC] abc137 abc137_c

---
+++ atcoder.jp/abc137/abc137_c/Main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using VI = vector<int>;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define all(obj) (obj).begin(), (obj).end()
+constexpr int DX[] = {1, 0, -1, 0};
+constexpr int DY[] = {0, 1, 0, -1};
+// const long long INF = numeric_limits<long long>::max();
+constexpr int INF = 1 << 30;
+
+int main()
+{
+	int n;
+	cin >> n;
+	
+	map<string, ll> num;
+	rep(i,n){
+		string s;
+		cin >> s;
+		
+		sort(all(s));
+
+		num[s]++;
+	}
+
+	ll ans = 0;
+	for(auto v : num){
+		ll x = v.second;
+		ans += x * (x - 1) / 2;
+	}
+	cout << ans << endl;
+	return 0;
+}
