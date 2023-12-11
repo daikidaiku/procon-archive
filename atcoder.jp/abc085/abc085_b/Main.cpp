7a34cbb53ace614486c6dd5ae737fab3e1fae03a
daikidaiku <daikimori990407@gmail.com>
2023-12-11 02:22:24 +0000

    [AC] abc085 abc085_b

---
+++ atcoder.jp/abc085/abc085_b/Main.cpp
@@ -0,0 +1,30 @@
+// map
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using VI = vector<int>;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define all(obj) (obj).begin(), (obj).end()
+constexpr int DX[] = {1, 0, -1, 0};
+constexpr int DY[] = {0, 1, 0, -1};
+// const long long INF = numeric_limits<long long>::max();
+constexpr int INF = 1 << 30;
+
+int main()
+{
+	int n;
+	cin >> n;
+	
+	set<int> s;
+	rep(i,n){
+		int d;
+		cin >> d;
+		s.insert(d);
+	}
+
+	cout << s.size() << endl;
+
+	
+	return 0;
+}
