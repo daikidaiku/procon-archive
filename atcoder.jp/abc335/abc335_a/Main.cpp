abbdb11bd0de326a0e418ba29d97bb0af3b82ea4
daikidaiku <daikimori990407@gmail.com>
2024-01-06 12:02:41 +0000

    [AC] abc335 abc335_a

---
+++ atcoder.jp/abc335/abc335_a/Main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using VI = vector<int>;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define all(obj) (obj).begin(), (obj).end()
+constexpr int dx[] = {1, 0, -1, 0};
+constexpr int dy[] = {0, 1, 0, -1};
+// const long long INF = numeric_limits<long long>::max();
+constexpr int INF = 1 << 30;
+
+int main()
+{
+	string s;
+	cin >> s;
+	int n = s.size();
+	string ans = s.substr(0,n-1) + '4';
+	cout << ans << endl;
+	return 0;
+}
